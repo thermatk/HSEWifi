767a7974da2c40d447ec171df763f32ccdd8423c
thermatk <thermatk@thermatk.com>
2014-11-09 17:16:59 +0300

    Chinaphone fixes Tested by A. Urman

--- app/src/main/java/com/thermatk/android/l/hsewifi/WiFiReceiver.java
+++ app/src/main/java/com/thermatk/android/l/hsewifi/WiFiReceiver.java
@@ -13,17 +13,18 @@ public class WiFiReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i("HSEWIFI", "1. RECEIVED");
-        NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-        if (intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION) && networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+        WifiManager wifiMan = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        NetworkInfo networkInfo = intent.getParcelableExtra(wifiMan.EXTRA_NETWORK_INFO);
+        if (intent.getAction().equals(wifiMan.NETWORK_STATE_CHANGED_ACTION) && networkInfo.getState() == NetworkInfo.State.CONNECTED) {
             Log.i("HSEWIFI", "2. CONNECTED");
-            WifiInfo wifiInfo = intent.getParcelableExtra(WifiManager.EXTRA_WIFI_INFO);
+            WifiInfo wifiInfo = intent.getParcelableExtra(wifiMan.EXTRA_WIFI_INFO);
             if (wifiInfo == null) {
                 Log.i("HSEWIFI", "2.E wifiInfo==null");
                 return;
             }
             String wifiName = wifiInfo.getSSID();
             Log.i("HSEWIFI", "2. " + wifiName + " BSSID: " + wifiInfo.getBSSID() + " STRENGTH: " + WifiManager.calculateSignalLevel(wifiInfo.getRssi(), 4));
-            if (wifiName.equals("\"HSE\"")) {
+            if (wifiName.equals("\"HSE\"") || wifiName.equals("HSE")) {
                 Log.i("HSEWIFI", "3.A HSE");
                 Toast.makeText(context, context.getString(R.string.connectedto) + " " + "HSE", Toast.LENGTH_SHORT).show();
                 context.startService(new Intent(context, HSEConnect.class));
